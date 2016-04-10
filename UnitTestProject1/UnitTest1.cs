908890089ffbe53b2ea41b36cf62fda543cb5b02
Feodor Fitsner <feodor@appveyor.com>
2016-04-10 14:19:26 -0700

    Added test project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 5;
+            Assert.AreEqual(5, a);
+        }
+    }
+}
